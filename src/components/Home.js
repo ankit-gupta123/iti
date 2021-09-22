006dc15a1ffac6fd37516f262a09c193ecbbeec2
ankit <akroy292@gmail.com>
2021-09-22 17:07:19 +0530

    Changes 22-Sept

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div >
+           
+            <p>WELCOME TO ITI JAIL-ROAD</p>
+            <h1>It is a Home Page</h1>
+        </div>
+    )
+}
+
+export default Home
